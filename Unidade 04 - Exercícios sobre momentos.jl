1fddb529cb9c0a13893c01c5fa4cdb0aabc350c6
jefferds <jefferds@gmail.com>
2021-10-29 08:41:35 -0300

    Unidade 04

---
+++ Unidade 04 - Exercícios sobre momentos.jl
@@ -0,0 +1,58 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 04 - Exercícios sobre momentos.
+
+### Tema relacionado na ementa:
+
+1. Equilíbrio de corpos rígidos.
+6. Cálculo das Reações.
+ 
+> Capítulo 3 - Corpos rígidos e sistemas equivalentes de força.
+>
+> **Fonte:**  3. BEER, Ferdinand P. _**Mecânica vetorial para engenheiros : estática**_., 9ª Edição
+
+### Resultados de aprendizagem esperados
+
+✔ Estudar o momento sobre corpos rígidos.
+
+✔ Analisar forças sobre corpos rigidos.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 29/out/2021)*,
+ 
+- _Exercícios relativos Corpos rígidos e sistemas equivalentes de força._;
+
+🕔 3. Seleção um exercício para cada membro do grupo dos tópicos abaixo no capitulo 3 do Beer para apresentação. Entregar no clasroom os arquivos da solução dos exercícios (slides, julia, pluto, figuras, etc.) *(até 04/nov/2021)*;:
+
+2. 3.11 - Momento de uma força em relação a um dado eixo
+5. 3.17 - Redução de um sistema de forças a uma força e um binário
+7. 3.21 - Redução de um sistema de forças a um torsor
+
+🕙 4. Apresentações dos temas *(até 04/nov/2021)*;
+
+🕑 5. Avaliações *(até 05/nov/2021)* e
+
+🕒 6. Feedback (sala) *(até 05/nov/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
