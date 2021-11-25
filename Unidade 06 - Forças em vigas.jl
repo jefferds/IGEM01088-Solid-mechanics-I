6ac7512101806fa2208bb11deecf598b62648140
jefferds <jefferds@gmail.com>
2021-11-25 08:27:04 -0300

    Unidade 06

---
+++ Unidade 06 - Forças em vigas.jl
@@ -0,0 +1,54 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 06 - Forças em vigas.
+
+### Tema relacionado na ementa:
+
+3. Equilíbrio de corpos rígidos.
+4. Estruturas isostáticas.
+5. Forças distribuídas.
+6. Cálculo das Reações.
+
+> Capítulo 7 - Forças em vigas e cabos.
+
+> **Fonte:**  3. BEER, Ferdinand P. _**Mecânica vetorial para engenheiros : estática**_., 9ª Edição
+
+### Resultados de aprendizagem esperados
+
+✔ Analisar esforços internos em corpos rígidos (vigas).
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 25/nov/2021)*,
+ 
+- _**Esforços internas em vigas**_
+
+🕔 2. Estudar dos capitulos 7, seções 7.1 a 7.6, do Beer, selecionar um subtema e cada membro apresentar um exercicio do subtema apresentado. Fazer a apresentação do subtema e da solução dos exercícios. *(02/dez/2021 e 03/dez/2021)*;
+
+🕙 3. Apresentações os subtemas e a solução dos exercício *(18/nov/2021 e 19/nov/2021)*;
+
+🕑 4. Avaliações *(até 09/dez/2021)* e
+
+🕒 5. Feedback (sala) *(até 09/dez/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
