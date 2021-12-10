bbec1321fe42ee4484bb4921249e77de781a3c3c
jefferds <jefferds@gmail.com>
2021-12-10 09:35:25 -0300

    Unidade 07

---
+++ Unidade 07 - Exercícios de Momento de Inércia.jl
@@ -0,0 +1,52 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 07 - Exercícios de Momento de Inércia
+
+### Tema relacionado na ementa:
+
+8. Momento de inércia de áreas e centroide.
+
+> 9.2 Momento de segunda ordem, ou momento de inércia, de uma superfície
+> 9.3 Determinação do momento de inércia de uma superfície por integração 
+
+> **Fonte:**  3. BEER, Ferdinand P. _**Mecânica vetorial para engenheiros : estática**_., 9ª Edição
+
+### Resultados de aprendizagem esperados
+
+✔ Calcular momento de inércia (vigas).
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 10/dez/2021)*,
+ 
+- _**Exercícios de Momento de Inércia**_
+
+🕔 2. Cada membro do grupo deve fazer dois exercícios propostos de cálculo de momento de inércia (Exercícios 9.1 a 9.22). *(16/dez/2021 e 16/dez/2021)*;
+
+🕙 3. Apresentações os subtemas e a solução dos exercício *(16/dez/2021)*;
+
+🕑 4. Avaliações *(até 17/dez/2021)* e
+
+🕒 5. Feedback (sala) *(até 17/dez/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
